cecef6099f8fd5f419e72579717227f7d1a0e7c2
ManojLL <manojlakshan421@gmail.com>
2021-05-13 10:33:27 +0530

    add sing page

--- eo-frontend/src/views/singUpPage/singUp.js
+++ eo-frontend/src/views/singUpPage/singUp.js
@@ -1,152 +1,177 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+// @material-ui/core components
+import {makeStyles} from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import People from "@material-ui/icons/People";
+// core components
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardFooter from "components/Card/CardFooter.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
+import styles from "assets/jss/material-kit-react/views/loginPage.js";
 
-export default function SignUp() {
+import image from "assets/img/bg7.jpg";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles(styles);
+
+export default function SingUp(props) {
+    const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+    setTimeout(function () {
+        setCardAnimation("");
+    }, 700);
     const classes = useStyles();
-
+    const {...rest} = props;
     return (
-        <Container  maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password2"
-                                label="confirm password"
-                                type="password"
-                                id="password2"
-                                autoComplete="current-password2"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="/" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
+        <div>
+
+            <div
+                className={classes.pageHeader}
+                style={{
+                    backgroundImage: "url(" + image + ")",
+                    backgroundSize: "cover",
+                    backgroundPosition: "top center",
+                }}
+            >
+                <div className={classes.container}>
+                    <GridContainer justify="center">
+                        <GridItem xs={12} sm={12} md={5}>
+                            <Card className={classes[cardAnimaton]}>
+                                <form className={classes.form}>
+                                    <CardHeader color="primary" className={classes.cardHeader}>
+                                        <h4>Sing Up</h4>
+                                        <div className={classes.socialLine}>
+                                            <Button
+                                                justIcon
+                                                href="#pablo"
+                                                target="_blank"
+                                                color="transparent"
+                                                onClick={(e) => e.preventDefault()}
+                                            >
+                                                <i className={"fab fa-twitter"}/>
+                                            </Button>
+                                            <Button
+                                                justIcon
+                                                href="#pablo"
+                                                target="_blank"
+                                                color="transparent"
+                                                onClick={(e) => e.preventDefault()}
+                                            >
+                                                <i className={"fab fa-facebook"}/>
+                                            </Button>
+                                            <Button
+                                                justIcon
+                                                href="#pablo"
+                                                target="_blank"
+                                                color="transparent"
+                                                onClick={(e) => e.preventDefault()}
+                                            >
+                                                <i className={"fab fa-google-plus-g"}/>
+                                            </Button>
+                                        </div>
+                                    </CardHeader>
+
+
+                                    <CardBody>
+                                        <CustomInput
+                                            labelText="User Name..."
+                                            id="first"
+                                            formControlProps={{
+                                                fullWidth: true,
+                                            }}
+                                            inputProps={{
+                                                type: "text",
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <People className={classes.inputIconsColor}/>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+
+                                        <CustomInput
+                                            labelText="Email"
+                                            id="email"
+                                            formControlProps={{
+                                                fullWidth: true,
+                                            }}
+                                            inputProps={{
+                                                type: "email",
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <Icon className={classes.inputIconsColor}>
+                                                            email
+                                                        </Icon>
+                                                    </InputAdornment>
+                                                ),
+                                                autoComplete: "off",
+                                            }}
+                                        />
+                                        <CustomInput
+                                            labelText="Password"
+                                            id="pass"
+                                            formControlProps={{
+                                                fullWidth: true,
+                                            }}
+                                            inputProps={{
+                                                type: "password",
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <Icon className={classes.inputIconsColor}>
+                                                            lock_outline
+                                                        </Icon>
+                                                    </InputAdornment>
+                                                ),
+                                                autoComplete: "off",
+                                            }}
+                                        />
+
+                                        <CustomInput
+                                            labelText="Confirm Password"
+                                            id="passCom"
+                                            formControlProps={{
+                                                fullWidth: true,
+                                            }}
+                                            inputProps={{
+                                                type: "password",
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <Icon className={classes.inputIconsColor}>
+                                                            lock_outline
+                                                        </Icon>
+                                                    </InputAdornment>
+                                                ),
+                                                autoComplete: "off",
+                                            }}
+                                        />
+                                        <Grid container justify="flex-end">
+                                            <Grid item>
+                                                <Link href="/" variant="body2">
+                                                    Already have an account? Sing In
+                                                </Link>
+                                            </Grid>
+                                        </Grid>
+                                    </CardBody>
+                                    <CardFooter className={classes.cardFooter}>
+                                        <Button color="primary" size="lg">
+                                            Sing up
+                                        </Button>
+                                    </CardFooter>
+                                </form>
+                            </Card>
+                        </GridItem>
+                    </GridContainer>
+                </div>
             </div>
-            <Box mt={5}>
-                <Copyright />
-            </Box>
-        </Container>
+        </div>
     );
 }
